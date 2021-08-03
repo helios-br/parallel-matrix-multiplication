3cc9079ee84c8cb0e893e17878133b4beac1cadb
Mauricio Medeiros <mauricio@helius.org>
2021-08-02 23:14:13 -0300

    Pending code

--- src/cpp/AlgorithmsAnalysis.cpp
+++ src/cpp/AlgorithmsAnalysis.cpp
@@ -20,6 +20,8 @@ void executeAnalysis(InstanceData instanceData)
 
 	vector<int> orders;
 	vector<float> timesScenario1;
+	vector<float> timesScenario11;
+	vector<float> timesScenario12;
 	vector<float> timesScenario2;
 	vector<float> timesScenario3;
 	vector<float> timesScenario4;
@@ -38,6 +40,8 @@ void executeAnalysis(InstanceData instanceData)
 		cout << "\n##### New configuration. p = " << p << ", matrix order = " << matrixOrder << endl;
 
 		float processingTimesScenario1 = 0;
+		float processingTimesScenario11 = 0;
+		float processingTimesScenario12 = 0;
 		float processingTimesScenario2 = 0;
 		float processingTimesScenario3 = 0;
 		float processingTimesScenario4 = 0;
@@ -80,7 +84,59 @@ void executeAnalysis(InstanceData instanceData)
 			cout << "Scenario 1 [" << n << "]: " << time << " seconds" << endl;
 
 			/**
-			 * Scenario 2: using integer type
+			 * Scenario 11: naive algorithm (using int)
+			 */
+
+			startTime = high_resolution_clock::now();
+
+			// Execution
+
+			vector<vector<int>> resultMatrix11(matrixOrder, vector<int>(matrixOrder));
+
+			for (i = 0; i < matrixOrder; i++)
+				for (j = 0; j < matrixOrder; j++)
+					for (k = 0; k < matrixOrder; k++)
+						resultMatrix11[i][j] += matrixA[i][k] * matrixB[k][j];
+
+			// Execution time
+
+			duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
+			time = (float)duration / 1000000;
+			processingTimesScenario11 += time;
+			cout << "Scenario 1.1 [" << n << "]: " << time << " seconds" << endl;
+
+			/**
+			 * Scenario 12: naive algorithm (index)
+			 */
+
+			startTime = high_resolution_clock::now();
+
+			// Execution
+
+			vector<vector<int>> resultMatrix12(matrixOrder, vector<int>(matrixOrder));
+
+			for (i = 0; i < matrixOrder; i++)
+			{
+				for (j = 0; j < matrixOrder; j++)
+				{
+					int sum = 0;
+					for (k = 0; k < matrixOrder; k++)
+					{
+						sum += matrixA[i][k] * matrixB[k][j];
+					}
+					resultMatrix12[i][j] = sum;
+				}
+			}
+
+			// Execution time
+
+			duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
+			time = (float)duration / 1000000;
+			processingTimesScenario12 += time;
+			cout << "Scenario 1.2 [" << n << "]: " << time << " seconds" << endl;
+
+			/**
+			 * Scenario 2: loop inversion
 			 */
 
 			startTime = high_resolution_clock::now();
@@ -90,9 +146,17 @@ void executeAnalysis(InstanceData instanceData)
 			vector<vector<int>> resultMatrix2(matrixOrder, vector<int>(matrixOrder));
 
 			for (i = 0; i < matrixOrder; i++)
-				for (j = 0; j < matrixOrder; j++)
-					for (k = 0; k < matrixOrder; k++)
-						resultMatrix2[i][j] += matrixA[i][k] * matrixB[k][j];
+			{
+				for (k = 0; k < matrixOrder; k++)
+				{
+					int sum = 0;
+					for (j = 0; j < matrixOrder; j++)
+					{
+						sum += matrixA[i][k] * matrixB[k][j];
+					}
+					resultMatrix2[i][j] = sum;
+				}
+			}
 
 			// Execution time
 
@@ -102,36 +166,14 @@ void executeAnalysis(InstanceData instanceData)
 			cout << "Scenario 2 [" << n << "]: " << time << " seconds" << endl;
 
 			/**
-			 * Scenario 3: loop inversion
+			 * Scenario 3: blocking
 			 */
 
 			startTime = high_resolution_clock::now();
 
 			// Execution
 
-			vector<vector<int>> resultMatrix3(matrixOrder, vector<int>(matrixOrder));
-
-			for (i = 0; i < matrixOrder; i++)
-				for (k = 0; k < matrixOrder; k++)
-					for (j = 0; j < matrixOrder; j++)
-						resultMatrix3[i][j] += matrixA[i][k] * matrixB[k][j];
-
-			// Execution time
-
-			duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
-			time = (float)duration / 1000000;
-			processingTimesScenario3 += time;
-			cout << "Scenario 3 [" << n << "]: " << time << " seconds" << endl;
-
-			/**
-			 * Scenario 4: blocking
-			 */
-
-			startTime = high_resolution_clock::now();
-
-			// Execution
-
-			vector<vector<int>> resultMatrix4(matrixOrder, vector<int>(matrixOrder));;
+			vector<vector<int>> resultMatrix3(matrixOrder, vector<int>(matrixOrder));;
 
 			for (jj = 0; jj < matrixOrder; jj += bsize)
 			{
@@ -153,12 +195,12 @@ void executeAnalysis(InstanceData instanceData)
 					{						
 						for (j = jj; j < jEdge; j++)
 						{
-							int value = 0; // GREAT IDEA (LESS INDEXATIONS)
+							int sum = 0;
 							for (k = kk; k < kEdge; k++)
 							{
-								value += matrixA[i][k] * matrixB[k][j];
+								sum += matrixA[i][k] * matrixB[k][j];
 							}
-							resultMatrix4[i][j] += value;
+							resultMatrix3[i][j] += sum;
 						}
 					}
 				}
@@ -168,24 +210,26 @@ void executeAnalysis(InstanceData instanceData)
 
 			duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
 			time = (float)duration / 1000000;
-			processingTimesScenario4 += time;
-			cout << "Scenario 4 [" << n << "]: " << time << " seconds" << endl;
+			processingTimesScenario3 += time;
+			cout << "Scenario 3: " << time << " seconds" << endl;
 
 			/**
-			 * Scenario 5: parallel (blocking)
+			 * Scenario 4: parallel
 			 */
-
 			startTime = high_resolution_clock::now();
 
 			// Execution
 
-			vector<vector<int>> resultMatrix5(matrixOrder, vector<int>(matrixOrder));
+			vector<vector<int>> resultMatrix4(matrixOrder, vector<int>(matrixOrder));
 
 			omp_set_num_threads(8);
+			bsize = 32;
 
-			#pragma omp parallel for private(i, j, k, kk) shared(jj)
+			#pragma omp parallel for private(i, j, k, kk, jj)
 			for (jj = 0; jj < matrixOrder; jj += bsize)
 			{
+				//int numThreads = omp_get_num_threads();
+
 				int jEdge = jj + bsize;
 				if (matrixOrder < jEdge)
 				{
@@ -204,12 +248,12 @@ void executeAnalysis(InstanceData instanceData)
 					{
 						for (j = jj; j < jEdge; j++)
 						{
-							int value = 0; // GREAT IDEA (LESS INDEXATIONS)
+							int value = 0;
 							for (k = kk; k < kEdge; k++)
 							{
 								value += matrixA[i][k] * matrixB[k][j];
 							}
-							resultMatrix5[i][j] += value;
+							resultMatrix4[i][j] += value;
 						}
 					}
 				}
@@ -219,39 +263,16 @@ void executeAnalysis(InstanceData instanceData)
 
 			duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
 			time = (float)duration / 1000000;
-			processingTimesScenario5 += time;
-			cout << "Scenario 5 [" << n << "]: " << time << " seconds" << endl;
-
-			// Validation (all result matrices must be equal)
-
-			for (int a = 0; a < matrixOrder; a++)
-			{
-				for (int b = 0; b < matrixOrder; b++)
-				{
-					int value = resultMatrix2[a][b];
-					if (
-						resultMatrixDouble[a][b] != value ||
-						resultMatrix2[a][b] != value ||
-						resultMatrix3[a][b] != value ||
-						resultMatrix4[a][b] != value ||
-						resultMatrix5[a][b] != value)
-					{
-						cout << "resultMatrixDouble: " << resultMatrixDouble[a][b] << endl;
-						cout << "resultMatrix2: " << resultMatrix2[a][b] << endl;
-						cout << "resultMatrix3: " << resultMatrix3[a][b] << endl;
-						cout << "resultMatrix4: " << resultMatrix4[a][b] << endl;
-						cout << "resultMatrix5: " << resultMatrix5[a][b] << endl;
-						throw "Error";
-					}
-				}
-			}
+			processingTimesScenario4 += time;
+			cout << "Scenario 4: " << time << " seconds" << endl;
 
 			orders.push_back(matrixOrder);
 			timesScenario1.push_back(processingTimesScenario1 / instanceData.numberOfMatrixes);
+			timesScenario11.push_back(processingTimesScenario11 / instanceData.numberOfMatrixes);
+			timesScenario12.push_back(processingTimesScenario12 / instanceData.numberOfMatrixes);
 			timesScenario2.push_back(processingTimesScenario2 / instanceData.numberOfMatrixes);
 			timesScenario3.push_back(processingTimesScenario3 / instanceData.numberOfMatrixes);
 			timesScenario4.push_back(processingTimesScenario4 / instanceData.numberOfMatrixes);
-			timesScenario5.push_back(processingTimesScenario5 / instanceData.numberOfMatrixes);
 		}
 	}
 
@@ -266,10 +287,11 @@ void executeAnalysis(InstanceData instanceData)
 		cout << endl;
 		cout << "order = " << orders[index] << endl;
 		cout << "time  = " << timesScenario1[index] << " seconds" << endl;
-		cout << "time  = " << timesScenario2[index] << " seconds (double to integer)" << endl;
-		cout << "time  = " << timesScenario3[index] << " seconds (loop inversion)" << endl;
-		cout << "time  = " << timesScenario4[index] << " seconds (blocking)" << endl;
-		cout << "time  = " << timesScenario5[index] << " seconds (parallel blocking)" << endl;
+		cout << "time  = " << timesScenario11[index] << " seconds (double to integer)" << endl;
+		cout << "time  = " << timesScenario12[index] << " seconds (index)" << endl;
+		cout << "time  = " << timesScenario2[index] << " seconds (loop inversion)" << endl;
+		cout << "time  = " << timesScenario3[index] << " seconds (blocking)" << endl;
+		cout << "time  = " << timesScenario4[index] << " seconds (parallel)" << endl;
 	}
 
 	// Gnuplot
@@ -280,15 +302,17 @@ void executeAnalysis(InstanceData instanceData)
 	g1.set_ylabel("Time (seconds)");
 	g1.set_yautoscale();
 	g1.set_xautoscale();
-	g1.set_style("").plot_xy(orders, timesScenario1, "scenario 1");
-	g1.set_style("lines").plot_xy(orders, timesScenario1, "scenario 1");
-	g1.set_style("").plot_xy(orders, timesScenario2, "scenario 2 (double to int)");
-	g1.set_style("lines").plot_xy(orders, timesScenario2, "scenario 2 (double to int)");
-	g1.set_style("").plot_xy(orders, timesScenario3, "scenario 3 (loop inversion)");
-	g1.set_style("lines").plot_xy(orders, timesScenario3, "scenario 3 (loop inversion)");
-	g1.set_style("").plot_xy(orders, timesScenario4, "scenario 4 (blocking)");
-	g1.set_style("lines").plot_xy(orders, timesScenario4, "scenario 4 (blocking)");
-	g1.set_style("").plot_xy(orders, timesScenario5, "scenario 5 (parallel)");
-	g1.set_style("lines").plot_xy(orders, timesScenario5, "scenario 5 (parallel)");
+	g1.set_style("").plot_xy(orders, timesScenario1, "basico (double)");
+	g1.set_style("lines").plot_xy(orders, timesScenario1, "basico (double)");
+	g1.set_style("").plot_xy(orders, timesScenario11, "basico (int)");
+	g1.set_style("lines").plot_xy(orders, timesScenario11, "basico (int)");
+	g1.set_style("").plot_xy(orders, timesScenario12, "basico (menos indexacoes)");
+	g1.set_style("lines").plot_xy(orders, timesScenario12, "basico (menos indexacoes)");	
+	g1.set_style("").plot_xy(orders, timesScenario2, "loop inversion");
+	g1.set_style("lines").plot_xy(orders, timesScenario2, "loop inversion");	
+	g1.set_style("").plot_xy(orders, timesScenario3, "blocking");
+	g1.set_style("lines").plot_xy(orders, timesScenario3, "blocking");	
+	g1.set_style("").plot_xy(orders, timesScenario4, "8 threads");
+	g1.set_style("lines").plot_xy(orders, timesScenario4, "8 threads");	
 	waitForKey();
 }
--- src/cpp/ParallelAnalysis.cpp
+++ src/cpp/ParallelAnalysis.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <iomanip>
 #include "MatrixBuilder.h"
-//#include "GnuplotWrap.h"
+// #include "GnuplotWrap.h"
 #include <vector>
 #include "StringUtil.h"
 #include <omp.h>
@@ -16,11 +16,9 @@ void executeParallelAnalysis(InstanceData instanceData)
 {
 	// Execution
 
-	int initialP = 10;
+	int initialP = 1;
 
 	vector<int> orders;
-	vector<float> timesScenario1;
-	vector<float> timesScenario2;
 
 	high_resolution_clock::time_point startTime;
 	float duration;
@@ -30,6 +28,13 @@ void executeParallelAnalysis(InstanceData instanceData)
 	int bsize = 64;
 	vector<int> threads{1, 2, 4, 8};
 
+	vector<vector<float>> timesScenario1(threads.size());
+
+	for (int p = initialP; p <= instanceData.kMax; p++)
+	{
+		orders.push_back(pow(2, p));
+	}
+
 	for (unsigned t = 0; t < threads.size(); t++)
 	{
 		int numThreads = threads[t];
@@ -38,156 +43,108 @@ void executeParallelAnalysis(InstanceData instanceData)
 
 		omp_set_num_threads(numThreads);
 
-		for (int p = initialP, index = 0; p <= instanceData.kMax; p++, index++)
+		if (numThreads == 8)
+		{
+			bsize = 32;
+		}
+		else if (numThreads == 16)
+		{
+			bsize = 16;
+		}
+		else
+		{
+			bsize = 64;
+		}
+
+		for (int p = initialP; p <= instanceData.kMax; p++)
 		{
 			int matrixOrder = pow(2, p);
 			cout << "\n##### New configuration. p = " << p << ", matrix order = " << matrixOrder << endl;
 
 			float processingTimesScenario1 = 0;
-			float processingTimesScenario2 = 0;
 
-			for (int n = 0; n < instanceData.numberOfMatrixes; n++)
+			/**
+			 * Preparing data for execution
+			 */
+
+			vector<vector<int>> matrixA = createPopulatedMatrix(matrixOrder, instanceData.minElementValue, instanceData.maxElementValue);
+			vector<vector<int>> matrixB = createPopulatedMatrix(matrixOrder, instanceData.minElementValue, instanceData.maxElementValue);
+
+			/**
+			 * Scenario 1
+			 */
+			startTime = high_resolution_clock::now();
+
+			// Execution
+
+			vector<vector<int>> resultMatrix1(matrixOrder, vector<int>(matrixOrder));
+
+			#pragma omp parallel for private(i, j, k, kk, jj)
+			for (jj = 0; jj < matrixOrder; jj += bsize)
 			{
-				/**
-				 * Preparing data for execution
-				 */
+				//int numThreads = omp_get_num_threads();
 
-				vector<vector<int>> matrixA = createPopulatedMatrix(matrixOrder, instanceData.minElementValue, instanceData.maxElementValue);
-				vector<vector<int>> matrixB = createPopulatedMatrix(matrixOrder, instanceData.minElementValue, instanceData.maxElementValue);
-
-				/**
-				 * Scenario 1
-				 */
-				startTime = high_resolution_clock::now();
-
-				// Execution
-
-				vector<vector<int>> resultMatrix1(matrixOrder, vector<int>(matrixOrder));
-
-				#pragma omp parallel for private(i, j, k, jj, kk)
-				for (jj = 0; jj < matrixOrder; jj += bsize)
+				int jEdge = jj + bsize;
+				if (matrixOrder < jEdge)
 				{
-					int jEdge = jj + bsize;
-					if (matrixOrder < jEdge)
+					jEdge = matrixOrder;
+				}
+
+				for (kk = 0; kk < matrixOrder; kk += bsize)
+				{
+					int kEdge = kk + bsize;
+					if (matrixOrder < kEdge)
 					{
-						jEdge = matrixOrder;
+						kEdge = matrixOrder;
 					}
 
-					for (kk = 0; kk < matrixOrder; kk += bsize)
+					for (i = 0; i < matrixOrder; i++)
 					{
-						int kEdge = kk + bsize;
-						if (matrixOrder < kEdge)
+						for (j = jj; j < jEdge; j++)
 						{
-							kEdge = matrixOrder;
-						}
-
-						for (i = 0; i < matrixOrder; i++)
-						{
-							for (j = jj; j < jEdge; j++)
+							int value = 0;
+							for (k = kk; k < kEdge; k++)
 							{
-								int value = 0; // GREAT IDEA (LESS INDEXATIONS)
-								for (k = kk; k < kEdge; k++)
-								{
-									value += matrixA[i][k] * matrixB[k][j];
-								}
-								resultMatrix1[i][j] += value;
+								value += matrixA[i][k] * matrixB[k][j];
 							}
+							resultMatrix1[i][j] += value;
 						}
 					}
 				}
-
-				// Execution time
-
-				duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
-				time = (float)duration / 1000000;
-				processingTimesScenario1 += time;
-				cout << "Scenario 1 [" << n << "]: " << time << " seconds" << endl;
-
-				/**
-				 * Scenario 2
-				 */
-				startTime = high_resolution_clock::now();
-
-				// Execution
-
-				vector<vector<int>> resultMatrix2(matrixOrder, vector<int>(matrixOrder));
-
-				#pragma omp parallel for private(i, j, k, kk) shared(jj)
-				for (jj = 0; jj < matrixOrder; jj += bsize)
-				{
-					int jEdge = jj + bsize;
-					if (matrixOrder < jEdge)
-					{
-						jEdge = matrixOrder;
-					}
-
-					for (kk = 0; kk < matrixOrder; kk += bsize)
-					{
-						int kEdge = kk + bsize;
-						if (matrixOrder < kEdge)
-						{
-							kEdge = matrixOrder;
-						}
-
-						for (i = 0; i < matrixOrder; i++)
-						{
-							for (j = jj; j < jEdge; j++)
-							{
-								int value = 0; // GREAT IDEA (LESS INDEXATIONS)
-								for (k = kk; k < kEdge; k++)
-								{
-									value += matrixA[i][k] * matrixB[k][j];
-								}
-								resultMatrix2[i][j] += value;
-							}
-						}
-					}
-				}
-
-				// Execution time
-
-				duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
-				time = (float)duration / 1000000;
-				processingTimesScenario2 += time;
-				cout << "Scenario 2 [" << n << "]: " << time << " seconds" << endl;
-
-				// Validation (all result matrices must be equal)
-
-				for (int a = 0; a < matrixOrder; a++)
-				{
-					for (int b = 0; b < matrixOrder; b++)
-					{
-						if (resultMatrix2[a][b] != resultMatrix1[a][b])
-						{
-							cout << "resultMatrix1: " << resultMatrix1[a][b] << endl;
-							cout << "resultMatrix2: " << resultMatrix2[a][b] << endl;
-							throw "Error";
-						}
-					}
-				}
-
-				orders.push_back(matrixOrder);
-				timesScenario1.push_back(processingTimesScenario1 / instanceData.numberOfMatrixes);
-				timesScenario2.push_back(processingTimesScenario2 / instanceData.numberOfMatrixes);
 			}
+
+			// Execution time
+
+			duration = (duration_cast<microseconds>(high_resolution_clock::now() - startTime).count());
+			time = (float)duration / 1000000;
+			processingTimesScenario1 += time;
+			cout << "Scenario 1: " << time << " seconds" << endl;
+
+			
+
+			timesScenario1[t].push_back(processingTimesScenario1);
 		}
-
-		// Output results
-
-		cout << endl;
-		cout << "---- Processing time ----" << endl;
-		cout << std::fixed << std::setprecision(10);
-
-		for (unsigned index = 0; index < orders.size(); index++)
-		{
-			cout << endl;
-			cout << "order = " << orders[index] << endl;
-			cout << "time  = " << timesScenario1[index] << " seconds (64)" << endl;
-			cout << "time  = " << timesScenario2[index] << " seconds (32)" << endl;
-		}
-
-		orders.clear();
-		timesScenario1.clear();
-		timesScenario2.clear();
 	}
+
+	cout << "timesscenario: " << timesScenario1.size() << endl;
+	cout << "timesscenario size: " << timesScenario1[0].size() << endl;
+	cout << "orders size: " << orders.size() << endl;
+
+	// Gnuplot
+
+	/* Gnuplot g1("Results");
+	g1.set_grid();
+	g1.set_xlabel("Matrix order");
+	g1.set_ylabel("Time (seconds)");
+	g1.set_yautoscale();
+	g1.set_xautoscale();
+	g1.set_style("").plot_xy(orders, timesScenario1[0], "threads: 1");
+	g1.set_style("lines").plot_xy(orders, timesScenario1[0], "threads: 1");
+	g1.set_style("").plot_xy(orders, timesScenario1[1], "threads: 2");
+	g1.set_style("lines").plot_xy(orders, timesScenario1[1], "threads: 2");
+	g1.set_style("").plot_xy(orders, timesScenario1[2], "threads: 4");
+	g1.set_style("lines").plot_xy(orders, timesScenario1[2], "threads: 4");
+	g1.set_style("").plot_xy(orders, timesScenario1[3], "threads: 8");
+	g1.set_style("lines").plot_xy(orders, timesScenario1[3], "threads: 8"); */
+	waitForKey();
 }
